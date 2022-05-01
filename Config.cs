d57eb0aafafc136509e009d750dcf49f7ff0bd13
2vitreous <observer.in.a.box@gmail.com>
2022-05-01 17:34:27 +0200

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,8 @@
+﻿namespace annoying_quotes_bot;
+public class Config
+{
+    public const string BotUsername = "PajaPantsBotOrSomething";
+    public const string AccessToken = "SuperSecretToken";
+    public const string Channel = "RandomChannel";
+    public const int MessageFrequencyInSeconds = 1800;
+}
